29a9627aacb75e8ba295b938d42dc7b12163c159
Andy Rozk <avrozk@gmail.com>
2020-10-03 14:03:35 +0300

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Admin on 03.10.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
